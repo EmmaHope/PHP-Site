990fd552cab95e67a3bc57293e094450f849e85a
ELLINGSON EMMA <150065293@cart.ads>
2019-04-11 15:01:45 -0700

    text added'

--- Ellingson-Emma-topic.php
+++ Ellingson-Emma-topic.php
@@ -7,9 +7,20 @@
 <body>
     <?php 
     $a = "<h1>Martial Arts </h1>";
-    
+    $pic = "<p>I am goint to talk more specificly about Taekwondo. Taekwondo is , along with Judo, are apart of the Summer Olympic Games. Taekwondo is more of a kivking type of martial arts. I practice this art myself an dam currently on my way to becoming a black belt along with becoming an instructor. Taekwondo was introduced to America in the 1950's. In 1988, Taekwondo was staged at the Olympics in Seoul. The again in 1992 in Barcelona as a demonstartion sport. Then was adopted as an official program in the 2000 Sydney Olympics.</p>";
+    $mean = "<p>Taekwondo means : </p>";
+    $more = "<ul>
+                        <li>In Koean, Tae means foot</li>
+                        <li>Kwon means fist</li>
+                        <li>Do, in Korean, meaning way of</li>
+                    </ul> ";
+    $fin = "All together Taekwondo, in Korean, means way of the foot and fist.";
         
         echo $a;
+        echo $pic;
+        echo $mean;
+        echo $more;
+        echo $fin;
     ?>
 </body>
 </html>
