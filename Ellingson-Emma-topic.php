8926a1171196df069bd2bc11ed76737b71b1fabd
ELLINGSON EMMA <150065293@cart.ads>
2019-04-11 14:13:33 -0700

    first commit

---
+++ Ellingson-Emma-topic.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Emma's Topic</title>
+</head>
+<body>
+    <?php 
+    $a = "<h1>Martial Arts </h1>";
+    
+        
+        echo $a;
+    ?>
+</body>
+</html>
