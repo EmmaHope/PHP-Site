37002559b51479006ffd340e1ad263d03ad272ba
ELLINGSON EMMA <150065293@cart.ads>
2019-04-11 15:23:26 -0700

    everything done

--- Ellingson-Emma-topic.php
+++ Ellingson-Emma-topic.php
@@ -7,7 +7,7 @@
 <body>
     <?php 
     $a = "<h1>Martial Arts </h1>";
-    $pic = "<p>I am goint to talk more specificly about Taekwondo. Taekwondo is , along with Judo, are apart of the Summer Olympic Games. Taekwondo is more of a kivking type of martial arts. I practice this art myself an dam currently on my way to becoming a black belt along with becoming an instructor. Taekwondo was introduced to America in the 1950's. In 1988, Taekwondo was staged at the Olympics in Seoul. The again in 1992 in Barcelona as a demonstartion sport. Then was adopted as an official program in the 2000 Sydney Olympics.</p>";
+    $para = "<p>I am goint to talk more specificly about Taekwondo. Taekwondo is , along with Judo, are apart of the Summer Olympic Games. Taekwondo is more of a kivking type of martial arts. I practice this art myself an dam currently on my way to becoming a black belt along with becoming an instructor. Taekwondo was introduced to America in the 1950's. In 1988, Taekwondo was staged at the Olympics in Seoul. The again in 1992 in Barcelona as a demonstartion sport. Then was adopted as an official program in the 2000 Sydney Olympics.</p>";
     $mean = "<p>Taekwondo means : </p>";
     $more = "<ul>
                         <li>In Koean, Tae means foot</li>
@@ -15,12 +15,20 @@
                         <li>Do, in Korean, meaning way of</li>
                     </ul> ";
     $fin = "All together Taekwondo, in Korean, means way of the foot and fist.";
-        
+    $pic = "<img src='taekwondo-olympics.jpg' alt='photo from the 2008 summer Olympics taekwondo sparring match' />";
+    $tae = "<img src='simple-belt-rack.jpg' alt='Belt rack used ofr past belts in taekwondo' />";
+    $kwon = "<p>https://ymaa.com/articles/taekwondo-olympics-beijing-2008</p>";
+    $do = "<p>https://akross.ca/taekwondo-belt-rack</p>";
+    
         echo $a;
-        echo $pic;
+        echo $para;
         echo $mean;
         echo $more;
         echo $fin;
+        echo $pic;
+        echo $tae;
+        echo $kwon;
+        echo $do;
     ?>
 </body>
 </html>
